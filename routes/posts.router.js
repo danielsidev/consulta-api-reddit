edb1f75a77ef12d422c81c19a2550743f99e38ab
daniel <adeptodaniel@gmail.com>
2018-11-17 17:52:20 -0200

    REmoção de logs de teste; Melhoria no tratamento de erros.

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -6,12 +6,13 @@ let PostController = require('../controller/posts/post.controller');
 
 router.get('/type-order/:order',function(req,res,next){
     let typeOrder = req.params.order;
+    let data = {};
     if(typeOrder){
     let PostOrder = new PostController();    
     PostOrder.getPostsTypeOrderControl(typeOrder)
             .then((response) =>{
-                if(response.length >0){
-                    let data = { typeOrder:typeOrder};
+                if(response && response.length >0){
+                    data = { typeOrder:typeOrder};
                     successlog("Router: /posts/type-order ","We Found Posts in this period: "+data);
                     res.status(200).json({"response":200, "error":null, "body":response});
                 }else{
@@ -20,8 +21,14 @@ router.get('/type-order/:order',function(req,res,next){
                 }
             })
             .catch((error) => {
+                if(error!=={}){
                 errorlog("Router: /posts/type-order ",error);
                 res.status(400).json({"response":400, "error":"Is not possible to find now. Try again later.", "body":null});
+                }else{
+                    data = { typeOrder:typeOrder};
+                    successlog("Router: /posts/type-order ","We Not Found Posts in this period: "+data);
+                    res.status(200).json({"response":200, "message":"We Not Found Posts in this period!","error":null, "body":response});
+                }
             });
     }else{
         errorlog("Router: /posts/type-order ","Type Order Undefined or Null");
@@ -33,14 +40,14 @@ router.get('/period/type-order',function(req,res,next){
     let dtStart   = req.headers['x-access-dtstart'];
     let dtEnd     = req.headers['x-access-dtend'];
     let typeOrder = req.headers['x-access-type-order'];
-    
+    let data = {};
     if(dtStart && dtEnd && typeOrder){
 
     let Post = new PostController();
     Post.getPostsPeriodControl(dtStart, dtEnd, typeOrder)
             .then((response) =>{
-                if(response.length >0){
-                    let data = { dtStart:dtStart, dtEnd:dtEnd, typeOrder:typeOrder};
+                if(response && response.length >0){
+                    data = { dtStart:dtStart, dtEnd:dtEnd, typeOrder:typeOrder};
                     successlog("Router: /posts/period/type-order ","We Found Posts in this period: "+data);
                     res.status(200).json({"response":200, "error":null, "body":response});
                 }else{
@@ -49,8 +56,15 @@ router.get('/period/type-order',function(req,res,next){
                 }
             })
             .catch((error) => {
+                if(error!=={}){
                 errorlog("Router: /posts/period/type-order ",error);
                 res.status(400).json({"response":400, "error":"Is not possible to find now. Try again later.", "body":null});
+                }else{
+                    data = { dtStart:dtStart, dtEnd:dtEnd, typeOrder:typeOrder};
+                    successlog("Router: /posts/period/type-order ","We Not Found Posts in this period: "+data);
+                    res.status(200).json({"response":200, "message":"We Not Found Posts in this period!","error":null, "body":response});
+                }
+                
             });
                 }else{
                     errorlog("Router: /posts/period/type-order ","dtStart, dtEnd and  Type Order Undefined or Null");
