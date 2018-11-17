52c470889cad1a52ce80284773154760baf2b55f
daniel <adeptodaniel@gmail.com>
2018-11-17 17:17:32 -0200

    Atualizando SQL, remoção de logs de teste e pacotes não utilizados.

--- source/init.database.sql
+++ source/init.database.sql
@@ -8,15 +8,15 @@ use reddit;
 -- End - Database and User Database with Password and Privileges 
 
 -- Start - Table Post from Reddit
-CREATE TABLE `posts` (
-  `idposts` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `author` varchar(75) NOT NULL,
-  `ups` int(11) NOT NULL,
-  `comments` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `data_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE posts (
+  idposts int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title varchar(200) NOT NULL,
+  author varchar(75) NOT NULL,
+  ups int(11) NOT NULL,
+  comments int(11) NOT NULL,
+  created timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  data_created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 -- End - Table Post from Reddit
 
